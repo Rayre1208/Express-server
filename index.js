eb2483f59a4896f8132125913324998bcdf4122c
Wistorian <eazersupplicate@gmail.com>
2024-01-31 01:20:36 +0800

    MongoDB success

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-//const express = require('express');
+
 import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 const port = 3002;
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.set('strictQuery', true);
+
+
+async function connectToDatabase() {
+  await mongoose.connect(`${MONGODB_URI}`);
+  console.log('MongoDB connected successfully');
+}
 
 app.get('/', (req, res) => {
   res.send('Hello, Express!');
