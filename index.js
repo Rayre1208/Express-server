e84a14a264946536d31e7c0128730447b2b39ad8
Wistorian <eazersupplicate@gmail.com>
2024-01-30 18:30:54 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//const express = require('express');
+import express from 'express';
+const app = express();
+const port = 3002;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
