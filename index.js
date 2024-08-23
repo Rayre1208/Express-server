d8b1e8f6e62bec6a4582f80e84fa9c4fe4d167b8
Wistorian <eazersupplicate@gmail.com>
2024-08-24 07:53:58 +0800

    01

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
 const app = express();
 const port = 3002;
-//5500
+
 /*
 const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.set('strictQuery', true);
-
 
 async function connectToDatabase() {
   await mongoose.connect(`${MONGODB_URI}`);
   console.log('MongoDB connected successfully');
 };
 
-
 connectToDatabase();
 */
-app.get('/', (req, res) => {
-  res.send({ some: 'json' });
-});
 
+app.get("/", (req, res) => {
+  res.send({ some: "json" });
+});
+/*
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+*/
